886d751804a3488f74ed5d7a4f32035ca796237c
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2017-02-25 16:54:55 -0600

    Update imageProcess.py
    Fitting a model

--- imageProcess.py
+++ imageProcess.py
@@ -52,7 +52,7 @@ class imageProcess:
 
 
 		model = Sequential()
-		model.add(Convolution2D(32, 3, 3, input_shape=(img_width, img_height,3)))
+		model.add(Convolution2D(32, 3, 3, input_shape=(self.img_width, self.img_height,3)))
 		model.add(Activation('relu'))
 		model.add(MaxPooling2D(pool_size=(2, 2)))
 
@@ -70,6 +70,25 @@ class imageProcess:
 		model.add(Dropout(0.5))
 		model.add(Dense(1))
 		model.add(Activation('sigmoid'))
+
+		model.compile(loss='binary_crossentropy',
+			optimizer='rmsprop',
+			metrics=['accuracy'])
+
+
+
+		nb_epoch = 5
+		nb_train_samples = 5000
+		nb_validation_samples = 800
+
+
+
+		model.fit_generator(
+			train_generator,
+			samples_per_epoch=nb_train_samples,
+			nb_epoch=nb_epoch,
+			validation_data=validation_generator,
+			nb_val_samples=nb_validation_samples)
 
 
 
